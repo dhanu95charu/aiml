ab0bbbfc971599c745bf0ac21b3b28755aaf05e5
dhanu95charu <129049399+dhanu95charu@users.noreply.github.com>
2023-08-11 09:45:53 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,4 +1,5 @@
 num1 = 1.5
 num2 = 6.3
-sum = num1 + num2
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+num3 = 12
+sum = num1 + num2 + num3
+print('The sum of {0} and {1} is {2}'.format(num1, num2, num3, sum))
